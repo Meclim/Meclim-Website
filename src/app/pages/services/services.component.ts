69354a12c8dd151af7c60a3b4fc7964d878481f5
Lucas Yago <lucasyago63@gmail.com>
2024-12-04 16:00:59 -0300

    refatora  adicionando footer e header nas paginas

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../about/about.component";
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
