459517880a50bc68252bd946a4d6a9226d0ce399
Lucas Yago <lucasyago63@gmail.com>
2024-12-04 17:02:35 -0300

    implementa componente de titulo nas paginas

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { BreadcrumbComponent } from "../../components/breadcrumb/breadcrumb.component";
 
 @Component({
   selector: 'app-about',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BreadcrumbComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  pageTitle: string = "Empresa";
 }
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { BreadcrumbComponent } from "../../components/breadcrumb/breadcrumb.component";
 
 @Component({
   selector: 'app-services',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BreadcrumbComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+  pageTitle: string = "Serviços";
 
 }
