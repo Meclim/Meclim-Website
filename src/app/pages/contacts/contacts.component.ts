2aef918dad244ccebdc9b599ea0c7a7fc54dbd2b
Lucas Yago <lucasyago63@gmail.com>
2024-12-04 16:56:45 -0300

    cria e implementa o componente de titulo da pagina

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { BreadcrumbComponent } from "../../components/breadcrumb/breadcrumb.component";
 
 @Component({
   selector: 'app-contacts',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, BreadcrumbComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
 export class ContactsComponent {
-
+  pageTitle: string = "Contato";
 }
