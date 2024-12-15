ee0977830ef4bf800c9eb6afd4204569004bb967
Lucas Yago <lucasyago63@gmail.com>
2024-12-15 18:17:26 -0300

    correçao na rota inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,7 @@ import { ContactsComponent } from './pages/contacts/contacts.component';
 
 export const routes: Routes = [
 
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component:HomeComponent },
+  { path: '', component:HomeComponent },
   { path: 'empresa', component: AboutComponent},
   { path: 'servicos', component:ServicesComponent },
   { path: 'contatos', component:ContactsComponent },
